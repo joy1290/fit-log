f39cc344246e03541f641544695bdb12bc74f44f
joy1290 <joydhar1122@gmail.com>
2026-09-24 17:00:34 +0600

    Fix Problem

--- src/lib/workouts.ts
+++ src/lib/workouts.ts
@@ -1,21 +1,13 @@
+import fs from "fs/promises";
+import path from "path";
 import { Workout } from "@/types/workout.type";
 
 export async function getAllWorkouts(): Promise<Workout[]> {
-  const baseUrl = process.env.NEXT_PUBLIC_API_SERVER_BASE_URL;
+  const filePath = path.join(process.cwd(), "public", "data.json");
 
-  if (!baseUrl) {
-    throw new Error("NEXT_PUBLIC_API_SERVER_BASE_URL is not defined");
-  }
+  const file = await fs.readFile(filePath, "utf-8");
 
-  const res = await fetch(`${baseUrl}/data.json`, {
-    cache: "no-store",
-  });
-
-  if (!res.ok) {
-    throw new Error("Failed to fetch workouts");
-  }
-
-  const data = await res.json();
+  const data = JSON.parse(file);
 
   return data.workouts;
 }
