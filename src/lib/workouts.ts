7dab93d74589308194e4015e95714f572ae7bd15
joy1290 <joydhar1122@gmail.com>
2026-09-24 16:50:03 +0600

    Final Output

--- src/lib/workouts.ts
+++ src/lib/workouts.ts
@@ -1,12 +1,15 @@
 import { Workout } from "@/types/workout.type";
 
 export async function getAllWorkouts(): Promise<Workout[]> {
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_SERVER_BASE_URL}/data.json`,
-    {
-      cache: "no-store",
-    }
-  );
+  const baseUrl = process.env.NEXT_PUBLIC_API_SERVER_BASE_URL;
+
+  if (!baseUrl) {
+    throw new Error("NEXT_PUBLIC_API_SERVER_BASE_URL is not defined");
+  }
+
+  const res = await fetch(`${baseUrl}/data.json`, {
+    cache: "no-store",
+  });
 
   if (!res.ok) {
     throw new Error("Failed to fetch workouts");
